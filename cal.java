c6a6485cbba40891d6e499c1d2b7dd0e163a5018
unknown <shivamrajput81202@gmail.com>
2025-01-17 22:17:21 +0530

    ADD Multiply feature

--- cal.java
+++ cal.java
@@ -8,6 +8,9 @@ public static int sub(int a,int b){
 	return a-b;
 }
 
+public static int multiply(int a,int b){
+	return a*b;
+}
 public static void main(String args[]){ System.out.println("HELLO FROM");
 
 }
