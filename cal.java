850fca216a07acf0e521a347dc9eaabdc1258ff8
unknown <shivamrajput81202@gmail.com>
2025-01-17 22:10:14 +0530

    Subtraction Feature

--- cal.java
+++ cal.java
@@ -4,6 +4,9 @@ public static int add(int a,int b){
 
 	return a+b;
 }
+public static int sub(int a,int b){
+	return a-b;
+}
 
 public static void main(String args[]){ System.out.println("HELLO FROM");
 
